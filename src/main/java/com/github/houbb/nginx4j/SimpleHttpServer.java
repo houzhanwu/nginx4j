c0bfe48e3cf38f7a1620e56411675756ee3d1777
1358850447@qq.com <1358850447@qq.com>
2024-11-07 14:55:00 +0800

    package and conf

--- src/main/java/com/github/houbb/nginx4j/SimpleHttpServer.java
+++ src/main/java/com/github/houbb/nginx4j/SimpleHttpServer.java
@@ -1,25 +1,39 @@
 package com.github.houbb.nginx4j;
 
 import java.io.*;
-import java.net.*;
-import java.nio.file.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
 
 public class SimpleHttpServer {
 
     //http://localhost:8080/1.txt
     public static void main(String[] args) throws IOException {
-        int port = 8080; // 服务器监听的端口号
+        if (args.length < 2) {
+            System.err.println("Usage: java SimpleHttpServer <port> <basicPath>");
+            System.exit(1);
+        }
+        int port = 8080;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid port number: " + args[0]);
+            System.exit(1);
+        }
+
+        String basicPath = args[1];
+        //int port = 8080; // 服务器监听的端口号
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Listening on port " + port);
-
+        //String basicPath = "D:\\workspace_sx\\nginx4j\\src\\test\\resources";
         while (true) {
             Socket socket = serverSocket.accept();
             System.out.println("Accepted connection from " + socket.getRemoteSocketAddress());
-            handleClient(socket);
+            handleClient(socket, basicPath);
         }
     }
 
-    private static void handleClient(Socket socket) {
+    private static void handleClient(Socket socket, String basicPath) {
         try {
             InputStream input = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
@@ -29,7 +43,7 @@ public class SimpleHttpServer {
             String path = parts[1];
             String protocol = parts[2];
 
-            final String basicPath = "D:\\workspace_sx\\nginx4j\\src\\test\\resources/";
+            //final String basicPath = "D:\\workspace_sx\\nginx4j\\src\\test\\resources/";
             // 只处理GET请求
             if ("GET".equalsIgnoreCase(method)) {
                 File file = new File(basicPath + path);
